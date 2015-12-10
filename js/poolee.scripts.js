a7926490f31147c12f3583d0b2b5f0e2e274f349
ntnvu <ngtrngocvu@gmail.com>
2015-12-10 15:00:38 +0700

    update for menu on desktop

--- js/poolee.scripts.js
+++ js/poolee.scripts.js
@@ -14,29 +14,32 @@
             })
 
             //show hide menu on mobile
-            $(".show-nav").click(function(){
-                $(".swiper-nav ul").toggleClass("hide show");
-            });
-            $(".show-mobile-menu a").click(function(e){
-                e.preventDefault();
-                $("header").toggleClass("relative no-relative");
-                $(this).toggleClass("active");
-                $("#wrapper").toggleClass("wrapper-slide wrapper-slide-show");
-            });
-            $(".menu-item-has-children").each(function(){
-                var ul = $(this).children("ul");
-                ul.prepend("<li class='return-up'><a href=''>Back</a></li>");
-            });
-            $(".menu-item-has-children > a").click(function(e){
-                e.preventDefault();
-                $(this).next("ul").addClass("sub-menu-show");
-                var z_index = $(this).closest(".sub-menu-show").css("zIndex");
-                $(this).next("ul").css( "zIndex", z_index + 1);
-            });
-            $(".return-up a").on("click", function(e){
-                e.preventDefault();
-                $(this).closest(".sub-menu-show").removeClass("sub-menu-show");
-            });
+			if($(window).width() < 1200){
+				$(".show-nav").click(function(){
+					$(".swiper-nav ul").toggleClass("hide show");
+				});
+				$(".show-mobile-menu a").click(function(e){
+					e.preventDefault();
+					$("header").toggleClass("relative no-relative");
+					$(this).toggleClass("active");
+					$("#wrapper").toggleClass("wrapper-slide wrapper-slide-show");
+				});
+				$(".menu-item-has-children").each(function(){
+					var ul = $(this).children("ul");
+					ul.prepend("<li class='return-up'><a href=''>Back</a></li>");
+				});
+				$(".menu-item-has-children > a").click(function(e){
+					e.preventDefault();
+					$(this).next("ul").addClass("sub-menu-show");
+					var z_index = $(this).closest(".sub-menu-show").css("zIndex");
+					$(this).next("ul").css( "zIndex", z_index + 1);
+				});
+				$(".return-up a").on("click", function(e){
+					e.preventDefault();
+					$(this).closest(".sub-menu-show").removeClass("sub-menu-show");
+				});
+			}
+            
 
             //show hide sidebar list
             if($("#slide:visible").length > 0){
